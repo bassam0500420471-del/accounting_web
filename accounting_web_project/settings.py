dc2e6d11a99fe066cab9877d160d27e00e093b62
Bassam <bassam@example.com>
2026-07-19 13:00:17 +0300

    Fix SMTP email settings

--- accounting_web_project/settings.py
+++ accounting_web_project/settings.py
@@ -212,16 +212,25 @@ STATICFILES_STORAGE = "whitenoise.storage.StaticFilesStorage"
 # ============================================================
 # EMAIL CONFIGURATION
 # ============================================================
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
-EMAIL_HOST_USER = "bassam0500420471@gmail.com"
-EMAIL_HOST_PASSWORD = "setvxdopwqjfguqv"
+EMAIL_HOST_USER = os.environ.get(
+    "EMAIL_HOST_USER",
+    "bassam0500420471@gmail.com"
+)
+
+EMAIL_HOST_PASSWORD = os.environ.get(
+    "EMAIL_HOST_PASSWORD",
+    "setvxdopwqjfguqv"
+)
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # ============================================================
