e790bfd367c95124eefb079b112ec0f7f72cce7d
Bassam <bassam@example.com>
2026-09-10 10:12:27 +0300

    Update POS payment and invoice

--- pos/models.py
+++ pos/models.py
@@ -112,7 +112,11 @@ class InvoiceItem(models.Model):
         Product,
         on_delete=models.CASCADE
     )
-    quantity = models.IntegerField()
+    quantity = models.DecimalField(
+    max_digits=10,
+    decimal_places=2,
+    default=0
+)
     price = models.DecimalField(
         max_digits=10,
         decimal_places=2
